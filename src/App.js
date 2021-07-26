17b6d70b1119cd3ad5145509e403cbe697c27d02
Shawlycode <kutinaorlando@gmail.com>
2021-07-26 19:01:58 +0000

    created states

--- src/App.js
+++ src/App.js
@@ -1,20 +1,51 @@
-import React, { Component } from 'react';
-import { Container, Row } from 'react-bootstrap';
-import AddEmployee from './Components/AddEmployee';
-import EmployeesData from './Components/EmployeesData';
+import React, { Component } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import EmployeesData from "./Components/EmployeesData";
+import AddEmployee from "./Components/AddEmployee";
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: []
+    }
+  }
+
+  addNewUser = (newData) => {
+    newData.id = Math.random().toString()
+    this.setState({
+      data: [...this.state.data, newData],
+    });
+  };
+  deleteUser = (id) => {
+    let savedUsers = this.state.data.filter((newData) => newData.id !==id);
+    this.setState({data: savedUsers });
+  };
+  handleEditUser = (updatedUser) =>{
+    this.setState({
+      data: this.state.data.map(user => user.id ===updatedUser.id ? updatedUser : user)
+    })
+
+  }
   render() {
     return (
       <>
-        <Container>
+        <Container fluid style={{ marginTop: "2rem" }}>
           <Row>
-            <AddEmployee />
-          </Row>
-        </Container>
-        <Container>
-          <Row>
-            <EmployeesData />
+            <Col>
+              <AddEmployee addNewData={this.addNewUser} />
+            </Col>
+            </Row>
+            <Row>
+            <Col>
+              <EmployeesData
+                userData={this.state.data}
+                deleteUser={this.deleteUser}
+                editUser={this.handleEditUser}
+              />
+            </Col>
           </Row>
         </Container>
       </>
